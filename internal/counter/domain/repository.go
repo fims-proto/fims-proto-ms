449642398d92af1f63b5647c5bcc602b358966fa
ldXiao <xld704@gmail.com>
2021-02-07 16:28:29 -0500

    premature Numbering service

---
+++ internal/counter/domain/repository.go
@@ -0,0 +1,20 @@
+package counter 
+
+import (
+	"context"
+	"fmt"
+)
+
+type Repository interface {
+	// maybe someday, reseting formater in Counter is necessary
+	AddCounter(ctx context.Context, c *Counter) error
+	GetNextFromCounter(
+		ctx context.Context,
+		counterUUID string,
+	) (string, error)
+	ResetCounter(
+		ctx context.Context,
+		counterUUID string,
+	) error
+
+}
