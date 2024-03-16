5436db0dfdffaab48f480e26c937f7e04ab9e8e9
Linfeng Ou <linfeng.ou@gmail.com>
2024-03-16 14:54:24 +0800

    Fix error in docker build

--- internal/general_ledger/domain/account/account.go
+++ internal/general_ledger/domain/account/account.go
@@ -3,6 +3,8 @@ package account
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/google/uuid"
 	"github/fims-proto/fims-proto-ms/internal/general_ledger/domain/account/balance_direction"
@@ -119,6 +121,28 @@ func NewByAllFields(
 		balanceDirection:    bd,
 		auxiliaryCategories: auxiliaryCategories,
 	}, nil
+}
+
+func composeAccountNumber(numberHierarchy, codeLengths []int) (string, error) {
+	if len(numberHierarchy) > len(codeLengths) {
+		return "", fmt.Errorf("account number hierarchy %d exceeds max depth %d", len(numberHierarchy), len(codeLengths))
+	}
+
+	for i := 0; i < len(numberHierarchy); i++ {
+		if numberHierarchy[i] < 1 {
+			return "", fmt.Errorf("account number %d at level %d cannot be smaller than 1", numberHierarchy[i], i)
+		}
+		if len(strconv.Itoa(numberHierarchy[i])) > codeLengths[i] {
+			return "", fmt.Errorf("account number %d at level %d exceeds max length (%d)", numberHierarchy[i], i, codeLengths[i])
+		}
+	}
+
+	var builder strings.Builder
+	for i, number := range numberHierarchy {
+		builder.WriteString(fmt.Sprintf("%0*d", codeLengths[i], number))
+	}
+
+	return builder.String(), nil
 }
 
 func (a *Account) Id() uuid.UUID {
